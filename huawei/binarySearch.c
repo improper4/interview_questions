e58db7d83f3b591043ba3e6614cb4edd440d1cf8
improper4 <improper4@gmail.com>
2013-09-08 23:23:21 +0800

    fix binarySearch

--- huawei/binarySearch.c
+++ huawei/binarySearch.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-int binsearch1(int* arr, int from, int to, int x){
-	int i;
-	int mid = (from + to) / 2;
-	while(from <= to){
-		if(arr[mid] < x){
-			return binsearch1(arr, mid +1, to, x);
-		}else if(arr[mid] > x){
-			return binsearch1(arr, from, mid -1, x);
-		}
-		return mid;
-	}
-	return -1;
-}
 
 int binsearch(int* arr, int len, int x){
-	
-	return binsearch1(arr, 0, len -1, x);
+	int from = 0, to = len -1;
+	while(to - from > 1){
+		int mid = (from + to)/2;
+		if(arr[mid] <= x){
+			from = mid;
+		}else {
+			to = mid;
+		}
+	}
+	return from;
 }
 
 int main(){
