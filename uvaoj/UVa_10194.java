b3828065d663f141289bd1503dffc6b83eb7caf0
improper4 <improper4@gmail.com>
2013-09-13 13:46:35 +0800

    fix 10914

--- uvaoj/UVa_10194.java
+++ uvaoj/UVa_10194.java
@@ -1,62 +1,61 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
-
 
 public class UVa_10194 {
-	static void track(String game, HashMap<String, Club> clubs){
+	static void track(String game, HashMap<String, Integer> clubMap, Club[] clubs){
 		String[] tmp = game.split("#");
 		String[] goal = tmp[1].split("@");
-		clubs.get(tmp[0]).update(0, Integer.parseInt(goal[0]),
+		clubs[clubMap.get(tmp[0])].update(Integer.parseInt(goal[0]),
 				Integer.parseInt(goal[1]));
-		clubs.get(tmp[2]).update(2, Integer.parseInt(goal[0]),
-				Integer.parseInt(goal[1]));
-		
+		clubs[clubMap.get(tmp[2])].update(Integer.parseInt(goal[1]),
+				Integer.parseInt(goal[0]));
 	}
-	public static void main(String[] args) throws FileNotFoundException {
-		System.setIn(new FileInputStream("t.in"));
-		Scanner in = new Scanner(System.in);
-		int T = Integer.parseInt(in.nextLine());
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in,"ISO-8859-1"));
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out, "ISO-8859-1"));
+		int T = Integer.parseInt(in.readLine());
 		for(int i = 0; i < T; i++){
 			if(i > 0)
-				System.out.println();
-			String Tur = in.nextLine();
-			System.out.println(Tur);
+				out.println();
+			String Tur = in.readLine();
+			out.println(Tur);
 			
-			int totTeams = Integer.parseInt(in.nextLine());
-			HashMap<String,Club> clubs = new HashMap<String,Club>();
-			ArrayList<Club> ans = new ArrayList<Club>();
+			int totTeams = Integer.parseInt(in.readLine());
+			HashMap<String,Integer> teamRank = new HashMap<String,Integer>();
+			Club[] ans = new Club[totTeams];
 			for(int j = 0; j < totTeams; j++){
-				String name = in.nextLine();
-				Club tmp = new Club(name);
-				ans.add(tmp);
-				clubs.put(name, tmp);
+				String name = in.readLine();
+				teamRank.put(name, j);
+				ans[j] = new Club(name);
 			}
 		
-			int totMatchs = Integer.parseInt(in.nextLine());
+			int totMatchs = Integer.parseInt(in.readLine());
 			for(int j = 0; j < totMatchs; j++){
-				String tmp = in.nextLine();
-				track(tmp, clubs);
+				String game = in.readLine();
+				track(game, teamRank, ans);
 			}
+			Arrays.sort(ans);
 			
-			Collections.sort(ans);
-			
-			for(int k = 0; k < ans.size(); k++){
-				Club c = ans.get(k);
-				System.out.printf("%d) %s %dp, %dg (%d-%d-%d), %dgd (%d-%d)\n",
-					k+1, c.name, c.pt, c.games, c.w, c.d, c.l, c.diff, c.goals, c.goaled);
+			for(int k = 0; k < ans.length; k++){
+				Club c = ans[k];
+				out.println(k+1 + ") "+ c.name +" "+c.pt+"p, "+ c.games
+					+"g ("+ c.w+ "-"+ c.d+"-"+ c.l+"), "+ c.diff+ "gd ("+ c.goals+"-"+ c.goaled+")");
 			}
 		}
+		in.close();
+		out.close();
 	}
 }
 
 class Club implements Comparable<Club>{
 	String name;
 	int pt = 0;
-	int wins = 0;
 	int diff = 0;
 	int goals = 0;
 	int games = 0;
@@ -66,22 +65,14 @@ class Club implements Comparable<Club>{
 		w = d= l = 0;
 		this.name = str;
 	}
-	void update(int state, int n1, int n2){
+	void update(int n1, int n2){
 		int tmp;
 		this.games++;
-		if(state == 0){
-			this.goals += n1;
-			this.goaled += n2;
-			this.wins += n1 > n2? 1: 0;
-			this.diff += n1 - n2;
-			tmp = n1 >n2? 3:(n1== n2? 1: 0);
-		}else{
-			this.goals += n2;
-			this.goaled += n1;
-			this.wins += n2 > n1? 1:0;
-			this.diff += n2 - n1;
-			tmp= n2 > n1? 3:(n2 == n1? 1: 0);
-		}
+		this.goals += n1;
+		this.goaled += n2;
+		this.diff = this.goals - this.goaled;
+		tmp = n1 >n2? 3:(n1== n2? 1: 0);
+	
 		if(tmp == 3) this.w++;
 		if(tmp == 1) this.d++;
 		if(tmp == 0) this.l++;
@@ -89,17 +80,11 @@ class Club implements Comparable<Club>{
 	}
 	@Override
 	public int compareTo(Club o) {
-		if(this.pt > o.pt) return -1;
-		if(this.pt < o.pt) return 1;
-		if(this.wins > o.wins) return -1;
-		if(this.wins < o.wins) return 1;
-		if(this.diff > o.diff) return -1;
-		if(this.diff < o.diff) return 1;
-		if(this.goals > o.goals) return -1;
-		if(this.goals < o.goals) return 1;
-		if(this.games < o.games) return -1;
-		if(this.games > o.games) return 1;
-		return this.name.compareTo(o.name);
+		if(this.pt != o.pt) return o.pt - this.pt;
+		if(this.w != o.w) return o.w -this.w;
+		if(this.diff != o.diff) return o.diff -this.diff ;
+		if(this.goals != o.goals) return o.goals- this.goals;
+		if(this.games != o.games) return this.games - o.games;
+		return this.name.toLowerCase().compareTo(o.name.toLowerCase());
 	}
-	
 }
